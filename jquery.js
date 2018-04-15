fd08429d663a06ea7f4db67167c0d1526344f8e5
LEEN KIM <leenkim@alumni.usc.edu>
2018-04-14 20:03:44 -0700

    First commit

---
+++ jquery.js
@@ -0,0 +1,49 @@
+// $(document).ready(function(){    
+//     $("div").click(function(){
+//         $(this).hide();
+//     });
+// });
+
+// $(document).ready(function(){    
+//     $("h1").click(function(){
+//         $(this).addClass("blue");
+//     });
+// });
+
+// $(document).ready(function(){    
+//     $("p").click(function(){
+//         $(this).css("color", "orange");
+//     });
+// });
+
+// $(document).ready(function(){    
+//     $("header").click(function(){
+//         $(this).css("color", "purple");
+//     });
+// });
+
+// $(document).ready(function(){    
+//     $(".p1").hover(function(){
+//         $(this).css("font-family", "Times New Roman");
+//     }, function(){
+//         $(this).css("font-family", "Georgia");
+//     });
+// });
+
+// $(document).ready(function(){    
+//     $("#rainbows").dblclick(function(){
+//         alert("You double clicked me!");
+//     });
+// });
+
+// $(document).ready(function(){    
+//     $("input").focus(function(){
+//        $(this).css("background-color", "lightblue");
+//     });
+// });
+
+$(document).ready(function(){    
+    $("input").focus(function(){
+       $(this).css("background-color", "lightblue");
+    });
+});
